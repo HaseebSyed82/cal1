41ce68ab0e7e1ea239ba4ab941df5211ff0f1839
HaseebSyed82 <63724529+HaseebSyed82@users.noreply.github.com>
2021-02-12 00:30:04 -0600

    Add files via upload
    Files Added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
+
+function App() {
+  return (
+    <>
+      <Calendar />
+    </>
+
+  );
+}
+
+export default App;
